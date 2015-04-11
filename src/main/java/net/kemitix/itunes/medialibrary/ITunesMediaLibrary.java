4340ec7ba2c8f379569c332839ab3423db99edcb
Paul Campbell <pcampbell@kemitix.net>
2015-04-11 12:42:29 +0100

    ITunesMediaLibrary: load ApplicationContext
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/itunes/medialibrary/ITunesMediaLibrary.java
+++ src/main/java/net/kemitix/itunes/medialibrary/ITunesMediaLibrary.java
@@ -3,15 +3,20 @@ package net.kemitix.itunes.medialibrary;
 import java.io.File;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Setter
 @Getter
 public class ITunesMediaLibrary {
 
+    private final ApplicationContext cx;
+
     private final MediaLibrary mediaLibrary;
 
     public ITunesMediaLibrary(File mediaLibraryFile) {
-        mediaLibrary = new MediaLibraryImpl();
+        cx = new AnnotationConfigApplicationContext(LibraryConfiguration.class);
+        mediaLibrary = cx.getBean(MediaLibrary.class);
         mediaLibrary.setLibraryFile(mediaLibraryFile);
     }
 
