411b2867d32c2b32b0ffd01ac90ee7a0af24596d
Paul Campbell <pcampbell@kemitix.net>
2015-04-19 20:02:22 +0100

    ITunesMediaLibrary: convert to return a MediaLibrary
    Rather than delegating all MediaLibrary methods, simply return the
    MediaLibrary object.
    
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/itunes/medialibrary/ITunesMediaLibrary.java
+++ src/main/java/net/kemitix/itunes/medialibrary/ITunesMediaLibrary.java
@@ -1,20 +1,12 @@
 package net.kemitix.itunes.medialibrary;
 
-import java.io.File;
-import java.util.List;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ITunesMediaLibrary {
 
-    private final AnnotationConfigApplicationContext cx;
-    private final MediaLibrary mediaLibrary;
-
-    public ITunesMediaLibrary(File mediaLibraryFile) {
-        cx = new AnnotationConfigApplicationContext(LibraryConfiguration.class);
-        mediaLibrary = cx.getBean(MediaLibrary.class);
-    }
-
-    List<Album> getAlbums() {
-        return mediaLibrary.getAlbums();
+    public static MediaLibrary createLibrary() {
+        AnnotationConfigApplicationContext cx
+                = new AnnotationConfigApplicationContext(LibraryConfiguration.class);
+        return cx.getBean(MediaLibrary.class);
     }
 }
