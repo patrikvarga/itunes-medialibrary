cc0484994fa8559e5249eac5d20f5759023a7125
Paul Campbell <pcampbell@kemitix.net>
2015-04-19 20:09:05 +0100

    MediaLibrary: add getItems() and getAlbumTracks() methods
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

--- src/main/java/net/kemitix/itunes/medialibrary/MediaLibrary.java
+++ src/main/java/net/kemitix/itunes/medialibrary/MediaLibrary.java
@@ -6,4 +6,8 @@ import java.util.List;
 public interface MediaLibrary {
 
     List<Album> getAlbums() throws SQLException;
+
+    List<Item> getItems() throws SQLException;
+
+    List<AlbumTracks> getAlbumTracks() throws SQLException;
 }
