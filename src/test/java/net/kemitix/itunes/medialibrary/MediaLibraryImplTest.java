c4e1e9a0a368472ce2a994cdfce995a599c16858
Paul Campbell <pcampbell@kemitix.net>
2015-04-20 15:42:53 +0100

    MediaLibraryImplTest: add test stub
    Signed-off-by: Paul Campbell <pcampbell@kemitix.net>

---
+++ src/test/java/net/kemitix/itunes/medialibrary/MediaLibraryImplTest.java
@@ -0,0 +1,63 @@
+package net.kemitix.itunes.medialibrary;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class MediaLibraryImplTest {
+
+    private MediaLibraryImpl mediaLibrary;
+
+    private AlbumDao albumDao;
+    private ArtistDao artistDao;
+    private ItemDao itemDao;
+    private AlbumTracksDao albumTracksDao;
+
+    @Before
+    public void setUp() {
+        albumDao = mock(AlbumDao.class);
+        artistDao = mock(ArtistDao.class);
+        itemDao = mock(ItemDao.class);
+        albumTracksDao = mock(AlbumTracksDao.class);
+        mediaLibrary
+                = new MediaLibraryImpl(albumDao, artistDao, itemDao, albumTracksDao);
+    }
+
+    @Test
+    public void testMocks() {
+        //given
+        AlbumDao albumDaoResult = mediaLibrary.getAlbumDao();
+        ArtistDao artistDaoResult = mediaLibrary.getArtistDao();
+        ItemDao itemDaoResult = mediaLibrary.getItemDao();
+        AlbumTracksDao albumTracksDaoResult = mediaLibrary.getAlbumTracksDao();
+
+        //then
+        assertThat(albumDaoResult, is(albumDao));
+        assertThat(artistDaoResult, is(artistDao));
+        assertThat(itemDaoResult, is(itemDao));
+        assertThat(albumTracksDaoResult, is(albumTracksDao));
+    }
+
+    @Test
+    public void testGetAlbums() throws SQLException {
+        //given
+        List<Album> albums = new ArrayList<>();
+        when(albumDao.selectAll()).thenReturn(albums);
+
+        //when
+        List<Album> result = mediaLibrary.getAlbums();
+
+        //then
+        assertThat(result, is(albums));
+    }
+
+}
