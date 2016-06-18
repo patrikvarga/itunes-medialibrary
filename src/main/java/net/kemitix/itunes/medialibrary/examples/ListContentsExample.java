9322d413bf11cd995ada3ff5c51de9f72b0b8c3b
Patrik Varga <varga.patrik@gmail.com>
2016-06-18 12:17:50 +1000

    Prettier print with track_number

--- src/main/java/net/kemitix/itunes/medialibrary/examples/ListContentsExample.java
+++ src/main/java/net/kemitix/itunes/medialibrary/examples/ListContentsExample.java
@@ -8,11 +8,9 @@ import net.kemitix.itunes.medialibrary.ITunesMediaLibrary;
 import net.kemitix.itunes.medialibrary.MediaLibrary;
 
 /**
- * Displays all the tracks in the test media library, grouped by artist and
- * album.
+ * Displays all the tracks in the test media library, grouped by artist and album.
  *
- * Where an individual track has a different artist the album, that is listed
- * beside the song title.
+ * Where an individual track has a different artist the album, that is listed beside the song title.
  *
  * @author pcampbell@kemitix.net
  */
@@ -21,26 +19,28 @@ public class ListContentsExample {
     private static final String FILE = "src/test/resources/MediaLibrary.sqlitedb";
 
     public static void main(String[] args) throws SQLException {
-        MediaLibrary library = ITunesMediaLibrary.createLibrary(FILE);
-        List<AlbumTrack> allTracks = library.getAlbumTracks();
-        System.out.format("Found %d album tracks\n", allTracks.size());
+        final MediaLibrary library = ITunesMediaLibrary.createLibrary(FILE);
+        final List<AlbumTrack> allTracks = library.getAlbumTracks();
+
+        System.out.format("Found %d album tracks\n\n", allTracks.size());
+
         allTracks.stream()
                 .collect(groupingBy(AlbumTrack::getAlbumArtist))
                 .forEach((String albumArtist, List<AlbumTrack> artistsTracks) -> {
                     System.out.println(albumArtist);
                     artistsTracks.stream()
-                    .collect(groupingBy(AlbumTrack::getAlbumTitle))
-                    .forEach((String albumTitle, List<AlbumTrack> albumTracks) -> {
-                        System.out.println("\t" + albumTitle);
-                        albumTracks.stream()
-                        .forEach((AlbumTrack albumTrack) -> {
-                            if (albumArtist.equals(albumTrack.getTrackArtist())) {
-                                System.out.format("\t\t- %s\n", albumTrack.getTrackTitle());
-                            } else {
-                                System.out.format("\t\t- %s [[%s]]\n", albumTrack.getTrackTitle(), albumTrack.getTrackArtist());
-                            }
-                        });
-                    });
+                            .collect(groupingBy(AlbumTrack::getAlbumTitle))
+                            .forEach((String albumTitle, List<AlbumTrack> albumTracks) -> {
+                                System.out.println("\t" + albumTitle);
+                                albumTracks.stream()
+                                        .forEach((AlbumTrack albumTrack) -> {
+                                            if (albumArtist.equals(albumTrack.getTrackArtist())) {
+                                                System.out.format("\t\t%02d. %s\n", albumTrack.getTrackNumber(), albumTrack.getTrackTitle());
+                                            } else {
+                                                System.out.format("\t\t%02d. %s - %s\n", albumTrack.getTrackNumber(), albumTrack.getTrackArtist(), albumTrack.getTrackTitle());
+                                            }
+                                        });
+                            });
                 });
     }
 }
