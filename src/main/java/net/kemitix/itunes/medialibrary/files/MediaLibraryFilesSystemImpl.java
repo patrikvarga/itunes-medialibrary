70ddcb90f13e5f5187471540432be9188b1a322f
Patrik Varga <varga.patrik@gmail.com>
2016-06-20 20:20:50 +1000

    Start file listing impl

--- src/main/java/net/kemitix/itunes/medialibrary/files/MediaLibraryFilesSystemImpl.java
+++ src/main/java/net/kemitix/itunes/medialibrary/files/MediaLibraryFilesSystemImpl.java
@@ -1,14 +1,21 @@
 package net.kemitix.itunes.medialibrary.files;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import net.kemitix.itunes.medialibrary.MediaLibrary;
 import net.kemitix.itunes.medialibrary.ReadOnly;
 import net.kemitix.itunes.medialibrary.items.Album;
 import net.kemitix.itunes.medialibrary.items.AlbumTrack;
 import net.kemitix.itunes.medialibrary.items.Artist;
 import net.kemitix.itunes.medialibrary.items.Item;
-import net.kemitix.itunes.medialibrary.v5.DbVersion5;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -38,7 +45,22 @@ public class MediaLibraryFilesSystemImpl implements MediaLibrary {
     @Override
     public List<AlbumTrack> getAlbumTracks() {
         // TODO getAlbumTracks from directories
-        throw new UnsupportedOperationException("Not supported yet.");
+        return listFiles(Paths.get(mediaLibraryFilePath)).stream()
+                .filter(MediaLibraryFilesSystemImpl::isMediaFile)
+                .map(MediaLibraryFilesSystemImpl::toAlbumTrack)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isMediaFile(Path path) {
+        return true;
+    }
+
+    private static AlbumTrack toAlbumTrack(Path path) {
+        final AlbumTrack track = new AlbumTrack();
+        track.setBaseLocation(path.getParent().toString());
+        track.setFileLocation(path.getFileName().toString());
+        // TODO read ID3 tag and fill info
+        return track;
     }
 
     @Override
@@ -46,4 +68,19 @@ public class MediaLibraryFilesSystemImpl implements MediaLibrary {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    private List<Path> listFiles(Path path) {
+        final List<Path> files = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listFiles(entry);
+                }
+                files.add(entry);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        Collections.sort(files);
+        return files;
+    }
 }
