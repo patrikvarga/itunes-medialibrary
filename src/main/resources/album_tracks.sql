203d032d2c812a450164707c49ca5ca59eec8b51
Patrik Varga <varga.patrik@gmail.com>
2016-06-25 02:00:24 +1000

    Use LEFT OUTER JOINs to allow null album artist etc

--- src/main/resources/album_tracks.sql
+++ src/main/resources/album_tracks.sql
@@ -12,12 +12,12 @@ SELECT
     genre.genre
 FROM
     item
-INNER JOIN item_artist ON item.item_artist_pid = item_artist.item_artist_pid
-INNER JOIN item_extra ON item_extra.item_pid = item.item_pid
-INNER JOIN album ON item.album_pid = album.album_pid
-INNER JOIN album_artist ON album.album_artist_pid = album_artist.album_artist_pid
-INNER JOIN item_video ON item.item_pid = item_video.item_pid
-INNER JOIN base_location ON item.base_location_id = base_location.base_location_id
-INNER JOIN genre ON item.genre_id = genre.genre_id
+LEFT OUTER JOIN item_artist ON item.item_artist_pid = item_artist.item_artist_pid
+LEFT OUTER JOIN item_extra ON item_extra.item_pid = item.item_pid
+LEFT OUTER JOIN album ON item.album_pid = album.album_pid
+LEFT OUTER JOIN album_artist ON album.album_artist_pid = album_artist.album_artist_pid
+LEFT OUTER JOIN item_video ON item.item_pid = item_video.item_pid
+LEFT OUTER JOIN base_location ON item.base_location_id = base_location.base_location_id
+LEFT OUTER JOIN genre ON item.genre_id = genre.genre_id
 ORDER BY
     album_artist, album, track_number, title
